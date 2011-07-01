4c672af960e9bee05b0934d065e409dda9fa3d7c
fazreil <fazreil@gmail.com>
2011-07-01 20:45:32 +0800

    can watermark

--- fazreil.scm
+++ fazreil.scm
@@ -9,15 +9,21 @@
 	(let *
 		(;variable declarations
 			;new layer for the image
-			(img (car (gimp-image-new 10 10 RGB)))
 			(ori (car (file-png-load 1 file file)))
-			;(text (car (gimp-text-fontname -1 -1 (/ (gimp-image-height ori) 2) (/ (gimp-image-width ori) 2) watermark 0 1 10 1 "Sans")))
-
+			(img (car (gimp-image-new 100 100 RGB)))
+			(imagewidth (/ (car(gimp-image-width (car(file-png-load 1 file file)))) 2) )
+			(imageheight (/ (car(gimp-image-height (car(file-png-load 1 file file)))) 2) )
 		)
 		;display the image
+		(let *(
+			(textlayer (car (gimp-text-fontname ori -1 (- imagewidth 100) (- imageheight 100) watermark 0 1 100 1 "Sans")))
+			)
+			(gimp-drawable-transform-rotate-default textlayer 45 1 imagewidth imageheight 1 0)
+		)
 		(gimp-display-new ori)
-		(gimp-text-fontname -1 -1 (/ (car(gimp-image-height ori)) 2) (/ (car(gimp-image-width ori)) 2) watermark 0 1 10 1 "Sans")
+		
 	)
+	
 
 )
 
@@ -32,5 +38,5 @@
    SF-STRING "Watermark" "Conto"      	            ; default parameters
    SF-DIRNAME "Original Image Directory" "/"
    SF-DIRNAME "Output folder" "/"
-   SF-FILENAME "File input" "D:/karja/GIMPFU/Automation/Before"
+   SF-FILENAME "File input" "D:/karja/GIMPFU/Automation/Before/Homepage"
 )
