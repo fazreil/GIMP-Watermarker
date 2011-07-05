7b57b982002671224d0d9e20bbae639612314a3f
fazreil <fazreil@gmail.com>
2011-07-05 15:49:31 +0800

    gold version

--- fazreil_batch.scm
+++ fazreil_batch.scm
@@ -12,22 +12,22 @@
 	(gimp-image-height (car(file-png-load 1 file file)))
 )
 
-(define (stamp-watermark ori imagewidth imageheight watermark)
+(define (stamp-watermark ori imagewidth imageheight watermark font)
 	(let* 
 		(
-			(textlayer (car (gimp-text-fontname ori -1 (- imagewidth 100) (- imageheight 100) watermark 0 1 100 1 "Sans")))
+			(textlayer (car (gimp-text-fontname ori -1 (- imagewidth 100) (- imageheight 100) watermark 0 1 20 1 font)))
 		)
 		;rotate layer -45 degree
 		(gimp-drawable-transform-rotate-default textlayer (- 0 0.7845) 1 imagewidth imageheight 1 0)
 		;stamp the textlayer to the original image
 		(plug-in-bump-map 1 ori textlayer textlayer 135 45 3 0 0 0 0 1 0 0)
 		;set opacity to 50%1
-		(gimp-layer-set-opacity textlayer 50)
+		(gimp-layer-set-opacity textlayer 40)
 		(gimp-image-merge-down ori textlayer EXPAND-AS-NECESSARY)
 	)
 )
 
-(define (MI-BWatermarker watermark inputfolder outputfolder file)
+(define (MI-BWatermarker watermark inputfolder outputfolder font)
 	(let*
 		(
 			(filelist (cadr(file-glob (string-append inputfolder "\\*.png") 1)))
@@ -41,8 +41,8 @@
 					(imageheight (/ (car(get-file-height filename)) 2))
 				)
 				;set foreground color to white
-				(gimp-palette-set-foreground '(255 255 255))
-				(stamp-watermark ori imagewidth imageheight watermark)
+				(gimp-palette-set-foreground '(127 127 127))
+				(stamp-watermark ori imagewidth imageheight watermark font)
 				;(gimp-display-new ori)
 				(gimp-image-clean-all ori)
 				(file-png-save-defaults 1 ori (car (gimp-image-get-active-layer ori)) filename filename)
@@ -64,36 +64,5 @@
    SF-STRING "Watermark" "Conto"      	            ; default parameters
    SF-DIRNAME "Original Image Directory" "C:/karja/GIMPFU/Automation/Before"
    SF-DIRNAME "Output folder" "C:/karja/GIMPFU/Automation/Test"
-   SF-FILENAME "File input" "C:/karja/GIMPFU/Automation/Before/Homepage.PNG"
-)
-
-(define (LoopaDemo watermark inputfolder outputfolder file)
-	(let* 
-		(
-			(filelist (cadr(file-glob (string-append inputfolder "\\*.png") 1)))
-		)
-		(while (not (null? filelist))
-				(let*
-					(
-						(filename (car filelist))
-					)
-					(gimp-message filename)
-					(set! filelist (cdr filelist))
-				)
-		)
-		
-	)
-)
-(script-fu-register                                 ; I always forget these ...
-   "LoopaDemo"           		                    ; script name to register
-   "<Image>/MIMOS/L_oopa"     						; where it goes
-   "Watermarks the images found in the given path"  ; script description
-   "Fazreil Amreen bin Abdul Jalil"                 ; author
-   "Copyright 2011 by Fazreil.jalil; GNU GPL"  		; copyright
-   "2011-07-01"                                     ; date
-   ""												; image type
-   SF-STRING "Watermark" "Conto"      	            ; default parameters
-   SF-DIRNAME "Original Image Directory" "C:/karja/GIMPFU/Automation/Before"
-   SF-DIRNAME "Output folder" "C:/karja/GIMPFU/Automation/Test"
-   SF-FILENAME "File input" "C:/karja/GIMPFU/Automation/Before/Homepage.PNG"
+   SF-FONT "Font type" "Sans"
 )
