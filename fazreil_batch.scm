f8376a63688930150f0d51f6df886d23d1dc3aa2
fazreil <fazreil@gmail.com>
2011-07-06 23:14:53 +0800

    watermark centered

--- fazreil_batch.scm
+++ fazreil_batch.scm
@@ -12,13 +12,26 @@
 	(gimp-image-height (car(file-png-load 1 file file)))
 )
 
+(define (center-watermark image textlayer)
+	(gimp-layer-set-offsets textlayer (- (/ (car(gimp-image-width image)) 2) (/ (car(gimp-drawable-width textlayer)) 2)) (- (/ (car(gimp-image-height image)) 2) (/ (car(gimp-drawable-height textlayer)) 2)))
+)
+
+(define (watermark-size ori)
+	(* (* (car(gimp-image-width ori)) (car(gimp-image-height ori))) 0.0001)
+)
+
 (define (stamp-watermark ori imagewidth imageheight watermark font)
 	(let* 
 		(
-			(textlayer (car (gimp-text-fontname ori -1 (- imagewidth 100) (- imageheight 100) watermark 0 1 50 1 font)))
+			(textlayer (car (gimp-text-fontname ori -1 0 0 watermark 0 1 50 1 font)))
 		)
+		;(gimp-message (number->string (car(gimp-image-width ori))))
+		;(gimp-message (number->string (car(gimp-image-height ori))))
+		;(gimp-message (number->string (* (* (car(gimp-image-width ori)) (car(gimp-image-height ori))) 0.0001)))
 		;rotate layer -45 degree
 		(gimp-drawable-transform-rotate-default textlayer (- 0 0.7845) 1 imagewidth imageheight 1 0)
+		;move the layer center
+		(center-watermark ori textlayer)
 		;stamp the textlayer to the original image
 		(plug-in-bump-map 1 ori textlayer textlayer 135 45 3 0 0 0 0 1 0 0)
 		;set opacity to 50%1
@@ -42,10 +55,10 @@
 				)
 				;set foreground color to white
 				(gimp-palette-set-foreground '(127 127 127))
+				;(gimp-message filename)
 				(stamp-watermark ori imagewidth imageheight watermark font)
 				;(gimp-display-new ori)
 				(gimp-image-clean-all ori)
-				(gimp-message filename)
 				(file-png-save-defaults 1 ori (car (gimp-image-get-active-layer ori)) filename filename)
 			)
 			(set! filelist (cdr filelist))
