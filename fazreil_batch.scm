d33532a49bb67b79909ca4dcb4cce930e2ee397b
fazreil <fazreil@gmail.com>
2011-07-13 11:12:11 +0800

    ultimate version with default size = 60

--- fazreil_batch.scm
+++ fazreil_batch.scm
@@ -4,12 +4,12 @@
 ;			path of folder of the watermarked images
 ; output:	watermarked images in the given path
 
-(define (get-file-width file)
-	(gimp-image-width (car(file-png-load 1 file file)))
+(define (get-file-width ori)
+	(gimp-image-width ori)
 )
 
-(define (get-file-height file)
-	(gimp-image-height (car(file-png-load 1 file file)))
+(define (get-file-height ori)
+	(gimp-image-height ori)
 )
 
 (define (center-watermark image textlayer)
@@ -20,14 +20,13 @@
 	(* (* (car(gimp-image-width ori)) (car(gimp-image-height ori))) 0.0001)
 )
 
-(define (stamp-watermark ori imagewidth imageheight watermark font)
+(define (stamp-watermark ori imagewidth imageheight watermark font size)
 	(let* 
 		(
-			(textlayer (car (gimp-text-fontname ori -1 0 0 watermark 0 1 50 1 font)))
+			(textlayer (car (gimp-text-fontname ori -1 0 0 watermark 0 1 size 1 font)))
 		)
-		;(gimp-message (number->string (car(gimp-image-width ori))))
-		;(gimp-message (number->string (car(gimp-image-height ori))))
-		;(gimp-message (number->string (* (* (car(gimp-image-width ori)) (car(gimp-image-height ori))) 0.0001)))
+		;move textlayer to top.
+		(gimp-image-raise-layer-to-top ori textlayer)
 		;rotate layer -45 degree
 		(gimp-drawable-transform-rotate-default textlayer (- 0 0.7845) 1 imagewidth imageheight 1 0)
 		;move the layer center
@@ -40,23 +39,23 @@
 	)
 )
 
-(define (MI-BWatermarker watermark inputfolder outputfolder font)
+(define (MI-BWatermarker watermark inputfolder font size)
 	(let*
 		(
-			(filelist (cadr(file-glob (string-append inputfolder "\\*.png") 1)))
+			(filelist (cadr(file-glob (string-append inputfolder "\\*.png") 0)))
 		)
 		(while (not(null? filelist))
 			(let*
 				(
 					(filename (car filelist))
 					(ori (car(file-png-load 1 filename filename)))
-					(imagewidth (/ (car(get-file-width filename)) 2))
-					(imageheight (/ (car(get-file-height filename)) 2))
+					(imagewidth (/ (car(get-file-width ori)) 2))
+					(imageheight (/ (car(get-file-height ori)) 2))
 				)
 				;set foreground color to white
 				(gimp-palette-set-foreground '(127 127 127))
 				;(gimp-message filename)
-				(stamp-watermark ori imagewidth imageheight watermark font)
+				(stamp-watermark ori imagewidth imageheight watermark font size)
 				;(gimp-display-new ori)
 				(gimp-image-clean-all ori)
 				(file-png-save-defaults 1 ori (car (gimp-image-get-active-layer ori)) filename filename)
@@ -67,16 +66,78 @@
 	)
 )
 
-(script-fu-register                                 ; I always forget these ...
-   "MI-BWatermarker"                                ; script name to register
-   "<Image>/MIMOS/BWatermarker"     				; where it goes
-   "Watermarks the images found in the given path"  ; script description
-   "Fazreil Amreen bin Abdul Jalil"                 ; author
-   "Copyright 2011 by Fazreil.jalil; GNU GPL"  		; copyright
-   "2011-07-01"                                     ; date
-   ""												; image type
-   SF-STRING "Watermark" "Conto"      	            ; default parameters
+(script-fu-register                                 					; I always forget these ...
+   "MI-BWatermarker"                                					; script name to register
+   "<Image>/MIMOS/_MI-Watermarker (PNG)"								; where it goes
+   "MIMOS Innovation: Watermarks the images found in the given path"  	; script description
+   "Fazreil Amreen bin Abdul Jalil"                 					; author
+   "Copyright 2011 by Fazreil.jalil; GNU GPL"  							; copyright
+   "2011-07-01"                                     					; date
+   ""																	; image type
+   SF-STRING "Watermark" "Sample"      	            					; default parameters
    SF-DIRNAME "Original Image Directory" "C:/karja/GIMPFU/Automation/Before"
-   SF-DIRNAME "Output folder" "C:/karja/GIMPFU/Automation/Test"
    SF-FONT "Font type" "Sans"
+   SF-ADJUSTMENT "Font Size" '(60 1 200 1 10 0 0)
+
+)
+
+(define (MI-JPGWatermarker watermark inputfolder font size)
+	(let*
+		(
+			(filelist (cadr(file-glob (string-append inputfolder "\\*.jpg") 0)))
+		)
+		(while (not(null? filelist))
+			(let*
+				(
+					(filename (car filelist))
+					(ori (car(file-jpeg-load 1 filename filename)))
+					(imagewidth (/ (car(get-file-width ori)) 2))
+					(imageheight (/ (car(get-file-height ori)) 2))
+				)
+				;set foreground color to white
+				(gimp-palette-set-foreground '(127 127 127))
+				;(gimp-message filename)
+				(stamp-watermark ori imagewidth imageheight watermark font size)
+				;(gimp-display-new ori)
+				(gimp-image-clean-all ori)
+				(file-jpeg-save 	1		 	ori 	(car (gimp-image-get-active-layer ori))	filename 											filename 											1 0 0 0 "Copyright MIMOS 2011" 						1 1 0 1)
+			)
+			(set! filelist (cdr filelist))
+		)
+		
+	)
+)
+
+(script-fu-register                                 					; I always forget these ...
+   "MI-JPGWatermarker"                                					; script name to register
+   "<Image>/MIMOS/_MI-Watermarker (JPG)"								; where it goes
+   "MIMOS Innovation: Watermarks the images found in the given path"  	; script description
+   "Fazreil Amreen bin Abdul Jalil"                 					; author
+   "Copyright 2011 by Fazreil.jalil; GNU GPL"  							; copyright
+   "2011-07-01"                                     					; date
+   ""																	; image type
+   SF-STRING "Watermark" "Sample"      	            					; default parameters
+   SF-DIRNAME "Original Image Directory" "C:/karja/GIMPFU/Automation/Before"
+   SF-FONT "Font type" "Sans"
+   SF-ADJUSTMENT "Font Size" '(60 1 200 1 10 0 0)
+)
+
+(define (MI-UWatermarker watermark inputfolder font size)
+	;the ultimate watermarker utility
+	(MI-JPGWatermarker watermark inputfolder font size)
+	(MI-BWatermarker watermark inputfolder font size)
+)
+
+(script-fu-register                                 					; I always forget these ...
+   "MI-UWatermarker"                                						; script name to register
+   "<Image>/MIMOS/_MI-Watermarker (Utlimate)"										; where it goes
+   "MIMOS Innovation: Watermarks the images found in the given path"  	; script description
+   "Fazreil Amreen bin Abdul Jalil"                 					; author
+   "Copyright 2011 by Fazreil.jalil; GNU GPL"  							; copyright
+   "2011-07-01"                                     					; date
+   ""																	; image type
+   SF-STRING "Watermark" "Sample"      	            					; default parameters
+   SF-DIRNAME "Original Image Directory" "C:/karja/GIMPFU/Automation/Before"
+   SF-FONT "Font type" "Sans"
+   SF-ADJUSTMENT "Font Size" '(60 1 200 1 10 0 0)
 )
